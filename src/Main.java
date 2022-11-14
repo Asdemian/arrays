d24b3e1e664e914ec94abf9b895e169f1d287422
hw commit <demaniks@yahoo.com>
2022-11-14 22:26:03 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         //task1
-
+        int[] aDozen = new int[12];
+        aDozen[0] = 1;
+        aDozen[1] = 2;
+        aDozen[2] = 3;
+        double[] aDozen2 = {1.57, 7.654, 9.986};
+        int[] aDozen3 = {5, 7, 10, 15};
     }
 }
