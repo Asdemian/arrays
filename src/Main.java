412f7af01b573e9e821c746c96bcbd915cf7b4f4
hw commit <demaniks@yahoo.com>
2022-11-14 23:12:39 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        //task2
+        //task3
         int[] aDozen = new int[3];
         aDozen[0] = 1;
         aDozen[1] = 2;
         aDozen[2] = 3;
-        for (int i = 0; i < aDozen.length-1; i++) {
-            System.out.print(aDozen[i]+ ", ");
+        for (int i = aDozen.length - 1; i > 0; i--) {
+            System.out.print(aDozen[i] + ", ");
         }
-        int o = aDozen.length-1;
-        System.out.println(aDozen[o]);
+        System.out.println(aDozen[0]);
         double[] aDozen2 = {1.57, 7.654, 9.986};
-        for (int i2 = 0; i2 < aDozen2.length-1; i2++) {
-            System.out.print(aDozen2[i2]+ ", ");
+        for (int i2 = aDozen2.length - 1; i2 > 0; i2--) {
+            System.out.print(aDozen2[i2] + ", ");
         }
-        int a = aDozen2.length-1;
-        System.out.println(aDozen2[a]);
+        System.out.println(aDozen2[0]);
         int[] aDozen3 = {5, 7, 10, 15, 7, 8};
-        for (int i3 = 0; i3 < aDozen3.length-1; i3++) {
-            System.out.print(aDozen3[i3]+ ", ");
+        for (int i3 = aDozen3.length - 1; i3 > 0; i3--) {
+            System.out.print(aDozen3[i3] + ", ");
         }
-        int b = aDozen3.length-1;
-        System.out.println(aDozen3[b]);
-
+        System.out.println(aDozen3[0]);
     }
 }
